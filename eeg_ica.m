d8820001dff88a64e48be31d8a85921409c99fc4
Matt Kmiecik <mkmiecik14@gmail.com>
2023-06-17 09:12:27 -0700

    updated pipeline from TARS to new project

---
+++ eeg_ica.m
@@ -0,0 +1,69 @@
+% EEG ICA Step 2 (after visual inspection)
+% Matt Kmiecik
+% Started 20 July 2022
+
+workspace_prep % Prepares workspace
+
+% Initializes subjects for batch processing (if applicable)
+ss = string({RAW{2:size(RAW,1),1}});
+i=1; % for testing purposes
+
+for i = 1:length(ss)
+    
+    % Creating variables ----
+    this_ss = ss{i};
+    this_ss_path = dir(fullfile(output_dir, strcat(this_ss, '*-prepro.set')));
+    this_ss_name = this_ss_path.name;
+    
+    % Checks to see if the a visually inspected and rejected file exists
+    % (i.e., if sections of the EEG were rejected due to noise)
+    this_ss_vis_rej = strcat(num2str(this_ss), '-prepro-vis-rej.set');
+    if isfile(fullfile(output_dir, this_ss_vis_rej))
+     % File exists...load in the visually inspected and rejected file
+     % Loads in raw data using EEGLAB ----
+    EEG = pop_loadset('filename',this_ss_vis_rej,'filepath', this_ss_path.folder);
+    else
+     % File does not exist...load standard file
+    % Loads in raw data using EEGLAB ----
+    EEG = pop_loadset('filename',this_ss_name,'filepath', this_ss_path.folder);
+    end
+    
+    % If there were any bad channels identified during inspection, they are
+    % imported here:
+    interpchans = str2num(RAW{i+1,2}); % gathers bad channels from excel
+    
+    % Interpolates bad channels if bad channels were identified
+    if sum(size(interpchans, 2)) > 0 % checks to see if there are bad channels
+            disp('Bad channel(s) detected...interpolating channels...');
+            EEG = pop_interp(EEG, interpchans, 'spherical');
+    else
+            disp('No bad channels detected...')
+    end
+    
+    % Extracts resting state blocks ----
+    %EEG = pop_rmdat( EEG, {'101','106','111','116'},[-1 61] ,0);
+    
+    % ICA decomposition ----
+    % computes rank from EEG.nbchan( == 30) - n (interpolated chans)
+    % minus 1 due to linked mastoids
+    %this_rank = EEG.nbchan - 1 - length(interpchans); 
+    this_rank = EEG.nbchan - length(interpchans); % for use w/ common avg.
+    EEG = pop_runica(...
+        EEG,...
+        'icatype', 'runica',... 
+        'extended',1,...
+        'interrupt','on',...
+        'pca',this_rank...
+    );
+    
+    % renames dataset
+    dataset_name = strcat(this_ss, '-ica');
+    EEG = pop_editset(EEG, 'setname', dataset_name, 'run', []);
+    
+    % Saves out data
+    outname = strcat(dataset_name, '.set'); % save out subject name
+    EEG = pop_saveset(EEG, 'filename', outname, 'filepath', output_dir);
+    
+end
+
+eeglab redraw
---
+++ eeg_prepro.m
@@ -0,0 +1,89 @@
+% EEG Preprocessing Pipeline Step 1
+% Matt Kmiecik
+% Started 03 June 2023
+
+workspace_prep % Prepares workspace (see /src)
+
+% Initializes subjects for batch processing (if applicable)
+ss = string({RAW{2:size(RAW,1),1}});
+
+i=1; % for testing purposes
+
+% Preprocessing ----
+for i = 1:length(ss)
+
+    % Creating variables ----
+    this_ss = ss{i};
+    this_ss_path = dir(fullfile(data_dir, strcat(this_ss, '.bdf')));
+    this_ss_name = this_ss_path.name;
+
+    % Loads in raw data using biosemi ----
+    EEG = pop_biosig(...
+        fullfile(this_ss_path.folder, this_ss_name),...
+        'ref',[1] ,...
+        'refoptions',{'keepref','on'},...
+        'importannot', 'off',... % does not import EDF annotations
+        'bdfeventmode', 6 ... % this event mode syncs nicely with EMSE events
+        );
+    
+    % Checks to see if an outside event file exists 
+    % (i.e., if sections of the EEG were rejected due to noise)
+    %this_events = strcat(num2str(this_ss), '-events.csv');
+    %if isfile(fullfile(data_dir, this_events))
+     % File exists...load in the visually inspected and rejected file
+     % Loads in raw data using EEGLAB ----
+     %disp('Using imported events...');
+    %events = readtable(fullfile(data_dir, this_events));
+    %events.latency = events.latency + 1; % add 1 as EMSE starts at 0
+    %EEG.event = table2struct(events); % inserts outside events
+    %else
+     % File does not exist...load standard file
+    % Loads in raw data using EEGLAB ----
+    %disp('Using embedded events...');
+    %end
+    
+    % Remove externals that are not being used ----
+    EEG = pop_select(...
+        EEG,...
+        'rmchannel',{'EXG1', 'EXG2', 'EXG3','EXG4','EXG5','EXG6','EXG7','EXG8'}...
+        );
+    
+    % Configuring channel locations ----
+    % loads in ELP
+    eloc = readlocs( this_elp ); % reads in elp chan locations
+    EEG.chanlocs = eloc(4:67); % adds these except for fiducials
+    % sets A1 as ref because it was chosen upon import
+    EEG = pop_chanedit(EEG, 'setref', {'1:64' 'A1'}); 
+    
+    % Downsamples to 256Hz ----
+    EEG = pop_resample(EEG, 256);
+    
+    % Re-references data to 
+    %EEG = pop_reref( EEG, [24 61] ,'keepref','on'); % linked mastoids
+    EEG = pop_reref( EEG, []); % common average reference
+    
+    % Removing DC offset by subtracting the mean signal from each electrode
+    EEG = pop_rmbase(EEG, [], []);
+    
+    % Highpass filter at 1Hz 
+    % -6dB @ 1Hz, 425 point highpass, 2Hz transition band width
+    EEG = pop_eegfiltnew(EEG, 'locutoff', 2, 'plotfreqz', 0);
+
+    % Cleanline ----
+    % Removing electrical line noise @ 60 Hz
+    EEG = pop_cleanline(EEG, 'bandwidth', 2, 'chanlist', [1:EEG.nbchan],...
+        'computepower', 1, 'linefreqs', 60, 'normSpectrum', 0, ...
+        'p', 0.01, 'pad', 2, 'plotfigures' , 0, 'scanforlines', 1, ...
+        'sigtype', 'Channels', 'tau', 100, 'verb', 1, 'winsize', ...
+        4,'winstep',1);
+    
+    % Renames dataset ----
+    dataset_name = strcat(this_ss, '-prepro');
+    EEG = pop_editset(EEG, 'setname', dataset_name, 'run', []);
+
+    % Saves out preprocessed data for inspection ----
+    EEG = pop_saveset(EEG, 'filename', dataset_name, 'filepath', output_dir);
+    
+    eeglab redraw % redraws to GUI for convenience
+
+end
