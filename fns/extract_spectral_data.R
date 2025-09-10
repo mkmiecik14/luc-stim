6001d9c3d0993ac978110cf80222108d2bc81670
Matt Kmiecik <mkmiecik14@gmail.com>
2025-09-09 19:30:37 -0700

    claude code improvements to function

--- fns/extract_spectral_data.R
+++ fns/extract_spectral_data.R
@@ -8,6 +8,14 @@ library(tidyverse)
 
 extract_spectral_data <- function(fpath){
   
+  # Input validation ----
+  if (!file.exists(fpath)) {
+    stop("File not found: ", fpath)
+  }
+  if (!grepl("\\.mat$", fpath, ignore.case = TRUE)) {
+    stop("File must have .mat extension: ", fpath)
+  }
+  
   # trigger definitions
   triggers <-
     tibble(
@@ -41,79 +49,65 @@ extract_spectral_data <- function(fpath){
   param.fw <- as.vector(d$param.fw) # SGF frame width
   param.poly <- as.vector(d$param.poly) # SGF polynomial order
   
+  # Helper function for array reshaping ----
+  reshape_array_data <- function(array, elec, freqs, block_names, value_name) {
+    dimnames(array) <- list(electrode = elec, frequency = freqs, block = block_names)
+    reshape2::melt(array, value.name = value_name) %>% 
+      as_tibble() %>%
+      rename(trigger = block) %>%
+      left_join(triggers, by = "trigger")
+  }
+  
   # PREALLOCATIONS ----
-  block_names <- as.vector(unlist(d$block.names)) # BLOCK NAMES
-  block_desc <- as.vector(unlist(d$block.descriptions)) # BLOCK DESCRIPTIONS
-  elec <- as.vector(unlist(d$meta.channels.after.interp)) # ELECTRODES
-  freqs <- as.vector(d$freqs[,1]) # FREQUENCIES
+  block_names <- as.vector(unlist(d$block.names))
+  elec <- as.vector(unlist(d$meta.channels.after.interp))
+  freqs <- as.vector(d$freqs[,1])
   
   # METADATA ----
-  meta.original.channels <- as.vector(d$meta.original.channels)
-  meta.final.channels <- as.vector(d$meta.final.channels)
-  meta.channels.before.interp <- as.vector(unlist(d$meta.channels.before.interp))
-  meta.channels.after.interp <- as.vector(unlist(d$meta.channels.after.interp))
-  meta.rejected.ics <- as.vector(d$meta.rejected.ics)
-  meta.rejected.ic.labels <- as.vector(unlist(d$meta.rejected.ic.labels))
-  meta.n.blocks.requested <- as.vector(d$meta.n.blocks.requested)
-  meta.n.blocks.processed <- as.vector(d$meta.n.blocks.processed)
-  meta.n.frequency.bins <- as.vector(d$meta.n.frequency.bins)
-  meta.frequency.resolution <- as.vector(d$meta.frequency.resolution)
-  meta.mean.psd.power <- as.vector(d$meta.mean.psd.power)
+  # Extract all metadata with consistent pattern
+  metadata <- list(
+    original.channels = as.vector(d$meta.original.channels),
+    final.channels = as.vector(d$meta.final.channels),
+    channels.before.interp = as.vector(unlist(d$meta.channels.before.interp)),
+    channels.after.interp = as.vector(unlist(d$meta.channels.after.interp)),
+    rejected.ics = as.vector(d$meta.rejected.ics),
+    rejected.ic.labels = as.vector(unlist(d$meta.rejected.ic.labels)),
+    n.blocks.requested = as.vector(d$meta.n.blocks.requested),
+    n.blocks.processed = as.vector(d$meta.n.blocks.processed),
+    n.frequency.bins = as.vector(d$meta.n.frequency.bins),
+    frequency.resolution = as.vector(d$meta.frequency.resolution),
+    mean.psd.power = as.vector(d$meta.mean.psd.power)
+  )
   
-  # BLOCK-WISE PSD IN dB ----
+  # SPECTRAL DATA ----
+  psd_db <- reshape_array_data(d$spectra.db, elec, freqs, block_names, "psd_db")
+  psd_uv <- reshape_array_data(d$spectra.psd, elec, freqs, block_names, "psd_uv")
   
-  # Add dimension names first
-  dimnames(d$spectra.db) <- 
-    list(electrode = elec, frequency = freqs, block = block_names)
+  # FREQUENCY METRICS ----
+  # Helper function for electrode-wise metrics
+  reshape_elec_data <- function(matrix, elec, block_names, value_name) {
+    rownames(matrix) <- elec 
+    colnames(matrix) <- block_names
+    as_tibble(matrix, rownames = "elec") %>%
+      pivot_longer(cols = -elec, names_to = "trigger", values_to = value_name) %>%
+      mutate(trigger = as.numeric(trigger)) %>%
+      left_join(triggers, by = "trigger")
+  }
   
-  # Melt to long format
-  psd_db <- 
-    reshape2::melt(d$spectra.db, value.name = "psd_db") %>% 
-    as_tibble() %>%
-    rename(trigger = block) %>%
-    left_join(., triggers, by = "trigger")
+  paf <- reshape_elec_data(d$paf, elec, block_names, "paf")
+  cog <- reshape_elec_data(d$cog, elec, block_names, "cog")
   
-  # BLOCK-WISE PSD in uV^2/Hz ----
-  
-  # Add dimension names first
-  dimnames(d$spectra.psd) <- 
-    list(electrode = elec, frequency = freqs, block = block_names)
-  
-  # Melt to long format
-  psd_uv <- 
-    reshape2::melt(d$spectra.psd, value.name = "psd_uv") %>% 
-    as_tibble() %>%
-    rename(trigger = block) %>%
-    left_join(., triggers, by = "trigger")
-  
-  # PAF ----
-  rownames(d$paf) <- elec 
-  colnames(d$paf) <- block_names 
-  paf <- 
-    as_tibble(d$paf, rownames = "elec") %>%
-    pivot_longer(cols = -elec, names_to = "trigger", values_to = "paf") %>%
-    mutate(trigger = as.numeric(trigger)) %>%
-    left_join(., triggers, by = "trigger")
-    
-  # COG ----
-  rownames(d$cog) <- elec 
-  colnames(d$cog) <- block_names
-  cog <- 
-    as_tibble(d$cog, rownames = "elec") %>%
-    pivot_longer(cols = -elec, names_to = "trigger", values_to = "cog") %>%
-    mutate(trigger = as.numeric(trigger)) %>%
-    left_join(., triggers, by = "trigger")
-  
-  # IAF ----
+  # IAF (individual alpha frequency) ----
   colnames(d$iaf) <- c("paf", "cog")
   rownames(d$iaf) <- block_names
   iaf <- 
     as_tibble(d$iaf, rownames = "trigger") %>%
     mutate(trigger = as.numeric(trigger)) %>%
-    left_join(., triggers, by = "trigger")
+    left_join(triggers, by = "trigger")
   
   # COMBINES INTO RESULTS ----
   res <- list(
+    # Core data
     subject = ssid,
     date = proc_time,
     psd_db = psd_db,
@@ -121,25 +115,19 @@ extract_spectral_data <- function(fpath){
     paf = paf,
     cog = cog,
     iaf = iaf,
+    
+    # Processing parameters
     param.wsize = param.wsize,
     param.overlap = param.overlap,
     param.frange = param.frange,
     param.alpha.window = param.alpha.window,
     param.cmin = param.cmin,
     param.fw = param.fw, 
-    param.poly = param.poly,
-    meta.original.channels = meta.original.channels, 
-    meta.final.channels  = meta.final.channels,
-    meta.channels.before.interp = meta.channels.before.interp, 
-    meta.channels.after.interp = meta.channels.after.interp,
-    meta.rejected.ics = meta.rejected.ics,
-    meta.rejected.ic.labels = meta.rejected.ic.labels,
-    meta.n.blocks.requested = meta.n.blocks.requested,
-    meta.n.blocks.processed  = meta.n.blocks.processed,
-    meta.n.frequency.bins = meta.n.frequency.bins,
-    meta.frequency.resolution = meta.frequency.resolution,
-    meta.mean.psd.power = meta.mean.psd.power
+    param.poly = param.poly
   )
+  
+  # Add metadata using list concatenation
+  res <- c(res, setNames(metadata, paste0("meta.", names(metadata))))
   
   cat("Finished!\n")
   
